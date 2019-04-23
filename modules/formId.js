5db9e8b7ce8fc626f6b35510e30f7c7f21240162
Leochens <18332518328@163.com>
2019-04-23 10:58:10 +0800

    fix

--- modules/formId.js
+++ modules/formId.js
@@ -7,21 +7,25 @@ client.auth('123456', function () {
 })
 
 // 为不同用户缓存form_id
-formId.addFormId = function (fid,u_id) {
-        if (fid === 'the formId is a mock one') return reject("the formId is a mock one");
-        console.log("前端发的fid",fid);
-        client.lpush('uid:'+u_id,fid,redis.print);
+formId.addFormId = function (fid, u_id) {
+    if (fid === 'the formId is a mock one') return reject("the formId is a mock one");
+    console.log("前端发的fid", fid);
+    client.lpush('uid:' + u_id, fid, redis.print);
 }
 // 得到用户的
 formId.getOne = function (u_id) {
     return new Promise(function (resolve, reject) {
-        client.brpop('uid:'+u_id,100,function(err,fid){
-            if (err) {
+        client.brpop('uid:' + u_id, 100, function (err, fid) {
+            if (err ) {
                 console.log(err);
                 return reject(err);
             }
+            if(!fid){
+                console.log("获得的fid为null");
+                return reject('获得的fid为null');
+            }
             // fid['key','value']
-            console.log("被选中的formid",fid[1]);
+            console.log("被选中的formid", fid[1]);
             return resolve(fid[1]);
         })
     })
