4b6893b2c939bcd08bd45ddf21ca411d789a4d45
Leochens <18332518328@163.com>
2019-04-23 10:19:00 +0800

    up

--- routes/form_id.js
+++ routes/form_id.js
@@ -6,11 +6,8 @@ const checkSession = require('../utils/checkSession');
 router.post('/formid',function(req,res){
     const formid = req.body.formid;
     const u_id = req.body.u_id;
-    formId.addFormId(formid,u_id).then(msg=>{
-        res.json(msg);
-    }).catch(err=>{
-        res.json(err);
-    });
+    formId.addFormId(formid,u_id);
+    res.json("看控制台");
 });
 
 router.get('/formid',function(req,res){
